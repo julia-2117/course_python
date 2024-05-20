d0ae7790d08b87e19767d09ce95bcc2ecf62bd54
julia-2117 <juliavanessagp03@hotmail.com>
2024-05-19 23:12:21 -0600

    Caesar_cipher

--- challenges/3. Caesar_cipher.py
+++ challenges/3. Caesar_cipher.py
@@ -9,9 +9,33 @@ values so that it can be used both to encode messages and decode messages.
 # When you have done the code, try to decrypt the following:
 ldl ndj zcdl wdl id iwxcz ndj wpkt xbegdkts ndjg eniwdc hzxaah rdcvgpijapixdch
 """
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
            's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 word = input('Enter a word: ')
+word = word.lower()
 shifts = int(input('How shifts you want?: '))
-Cifrado = int(input(''))
+
+
+def Julius(word, shifts, letters):
+    encript = ''
+    for i in word:
+        if i in letters:
+            pos = letters.index(i)
+            new = (pos+shifts) % len(letters)
+            encript += letters[new]
+        else:
+            encript += i
+    return encript
+
+
+print(f'Encrypt: {Julius(word, shifts, letters)}')
+
+
+def Julius_reverse(word, shifts, letters):
+    return Julius(word, -shifts, letters)
+
+
+word_encript = input('Enter the word you want to decrypt: ')
+decrypt = Julius_reverse(word_encript, shifts, letters)
+print(f'Decript: {decrypt}')
+
