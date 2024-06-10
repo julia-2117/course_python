485ba92d83311898bba875c4fcfe437f41bbefd1
julia-2117 <juliavanessagp03@hotmail.com>
2024-06-10 11:47:02 -0600

    hangman

--- challenges/c9_hangman/p9_game.py
+++ challenges/c9_hangman/p9_game.py
@@ -1,12 +1,11 @@
 from random import choice
-from hangman_art import logo
+from hangman_art import logo, stages
 from hangman_words import words
 print(logo)
 print('Welcome to the hangman game actuarial science version')
-
 word = choice(words)
 word = list(word)
-print(word)
+# print(word)
 p = []
 for _ in word:
     if _ != ' ':
@@ -15,10 +14,27 @@ for _ in word:
     else:
         print(' ', end='')
         p.append(' ')
-print('\n\n',p)
-g = input('\nEnter a guess: ')
-for i in word:
-    if g == i:
+#print('\n\n', p)
+
+
+def sust(list, g, s):
+    indice = len(s) - 1
+    for i in range(len(list)):
+        if list[i] == g:
+            p[i] = g
+        # elif list[i] != g:
+        #     print(s[indice])
+        #     indice -= 1
+    return p
+
+
+while '_' in p:
+    g = input('\nEnter a guess: ')
+    print(g)
+    s = stages
+    print(sust(word, g, s))
+
+
 
 
 
